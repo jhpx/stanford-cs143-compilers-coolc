ed502856ed6c6800ef48c9c373836cad9f87ff34
jhpx <jhpx@hotmail.com>
2014-08-11 16:08:07 +0800

    chmod +x some binary files.

--- install_on_debian.sh
+++ install_on_debian.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-chmod +x class/cs143/cool/bin/.i686/* && chmod -x class/cs143/cool/bin/.i686/*.*
+chmod +x class/cs143/cool/bin/.i686/*
 chmod +x class/cs143/cool/bin/* && chmod -x class/cs143/cool/bin/*.*
 chmod +x class/cs143/cool/etc/* && chmod -x class/cs143/cool/etc/*.*
 sudo apt-get install flex bison build-essential spim
