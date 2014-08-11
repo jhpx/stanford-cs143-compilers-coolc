cdc95cbf9494aa5632134855010d03b2bde2c10a
jhpx <jhpx@hotmail.com>
2014-08-11 11:05:14 +0800

    Rearrange Directories.

--- install_on_debian.sh
+++ install_on_debian.sh
@@ -1,7 +1,7 @@
 chmod +x class/cs143/cool/bin/.i686/* && chmod -x class/cs143/cool/bin/.i686/*.*
 chmod +x class/cs143/cool/bin/* && chmod -x class/cs143/cool/bin/*.*
 chmod +x class/cs143/cool/etc/* && chmod -x class/cs143/cool/etc/*.*
-sudo apt-get install spim
+sudo apt-get install flex bison build-essential spim
 sudo mkdir -p /usr/class/cs143/cool/lib/
 sudo chown $USER /usr/class/cs143/cool/lib/
 ln -s `pwd`/class/cs143/cool/lib/trap.handler /usr/class/cs143/cool/lib/trap.handler
